6daf5702b4e672686a135283870f6ecb4b81e778
Chethan Venkateshaiah <150097340+ChethanVenkateshaiah@users.noreply.github.com>
2024-01-15 18:25:20 +0000

    Update axios.jsx

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://api.themoviedb.org/3/discover/tv?api_key=3e2ccc31d1085ec88418cabd5fadeac1&with_networks=213",
+	baseURL: "https://api.themoviedb.org/3/",
 });
 
 export default instance;
